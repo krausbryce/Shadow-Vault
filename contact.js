d60bc081d19439234bbd330b3fb5f67bf545fcd3
krausbryce <krausb1@student.eit.ac.nz>
2025-10-31 19:26:07 +1300

    Refactor contact.js for improved message handling

--- contact.js
+++ contact.js
@@ -1,5 +1,5 @@
 // Import Supabase library and setup connection to database
-import { supabase } from 'initdb.js';
+import { supabase } from '../initdb.js';
 
 // Avoid errors
 function escapeHTML(str) {
@@ -90,3 +90,4 @@ document.getElementById('contactForm').addEventListener('submit', async function
   displayMessages();
 
 });
+
